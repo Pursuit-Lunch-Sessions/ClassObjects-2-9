5b02d26138389362bc65c55306f796d8e6fe4b4c
Kelvin <kelvinarellano@pursuit.org>
2020-02-10 21:48:00 -0500

    finished

--- index.js
+++ index.js
@@ -10,3 +10,40 @@ class Animal {
     return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
   }
 }
+
+class Shark extends Animal {
+  constructor(name, age, legs = 0, species="shark", status) {
+    super(name);
+    super(age);
+    this.legs = legs;
+    this.species = species;
+    super(status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, legs=4, species="Cat", status) {
+    super(name);
+    super(age);
+    this.legs = legs;
+    this.species = species;
+    super(status);
+  }
+  introduce(){
+    return super.introduce + "  " +"Meow meow!"
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, legs=4, species="dog", status, master){
+    super(name);
+    super(age);
+    this.legs = legs;
+    this.species=species;
+    this.status=status;
+    this.master=master;
+  }
+  greetMaster(){
+    return `Hello ${master}`
+  }
+}
